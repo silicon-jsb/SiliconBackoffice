b45de10cfee59de1b8b8f0977e46efec9ddc4396
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-05-28 20:15:24 +0200

    view courses working.

---
+++ NewBackoffice/NewBackoffice/Data/GraphQL/DynamicGraphQLResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+
+namespace NewBackoffice.Data.GraphQL;
+
+public class DynamicGraphQLResponse
+{
+    public JsonElement Data { get; set; }
+}
---
+++ NewBackoffice/NewBackoffice/Data/GraphQL/GraphQLQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace NewBackoffice.Data.GraphQL;
+
+public class GraphQLQuery
+{
+    public string Query { get; set; } = null!;
+    public object Variables { get; set; } = null!;
+}
